f567eafc98506b0c813dc7b46cad6f9e7c6ef816
Atul Ahire <atulahire9607@gmail.com>
2024-04-14 08:48:45 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import AddUser from'./components/Users/AddUser';
+import AddUser from './components/Users/AddUser';
 
 function App() {
   return (
     <div>
-    <AddUser />
+      <AddUser />
     </div>
-  )
+  );
 }
+
 export default App;
--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,18 +1,26 @@
 import React from 'react';
+import Card from '../UI/Card'; // Correct relative path to Card.js
+import Button from '../UI/Button';
+import classes from './AddUser.module.css'; 
 
-const AddUser =(props)=>{
-    const addUserHandler=(event) => {
-        event.preventDefault();
-    };
-    return(
-        <form onSubmit={addUserHandler}>
+const AddUser = (props) => {
+  const addUserHandler = (event) => {
+    event.preventDefault();
+    // Add logic to handle adding user
+  };
+
+  return (
+    <Card className={classes.input}>
+      <form onSubmit={addUserHandler}>
         <label htmlFor="username">Username</label>
-        <input id="username" type="text"/>
+        <input id="username" type="text" />
 
         <label htmlFor="age">Age(Years)</label>
-        <input id="age" type="number"/>
-        <button type="submit">Add User</button>
-        </form>
-    );
+        <input id="age" type="number" />
+        <Button type="submit">Add User</Button>
+      </form>
+    </Card>
+  );
 };
+
 export default AddUser;
